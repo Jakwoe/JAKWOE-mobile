866b97d686de1ec4a7a4df5b37caa75f9bcbdad3
Said Jumadil Akbar <38126175+saidjumadil@users.noreply.github.com>
2021-02-18 19:49:04 +0700

    Add files via upload

---
+++ Jakwoe/app/src/main/java/id/ac/unsyiah/android/jakwoe/MainActivity.java
@@ -0,0 +1,15 @@
+package id.ac.unsyiah.android.jakwoe;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    EditText email, password;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
